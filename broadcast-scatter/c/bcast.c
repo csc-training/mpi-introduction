0f8c1156749636144711c5e5085ca67dbb87df71
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2021-08-29 21:14:59 +0300

    Create bcast.c

---
+++ broadcast-scatter/c/bcast.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <mpi.h>
+
+
+void print_buffers(int *printbuffer, int *sendbuffer, int buffersize);
+void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize);
+
+
+int main(int argc, char *argv[])
+{
+    int ntasks, myid, color,size=12;
+    int sendbuf[ size], recvbuf[size];
+    int printbuf[size * size];
+
+    MPI_Comm sub_comm;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &myid);
+
+    /* Initialize message buffers */
+    init_buffers(sendbuf, recvbuf, size);
+
+    /* Print data that will be sent */
+    print_buffers(printbuf, sendbuf, size);
+
+    /* Send  everywhere */
+    MPI_Bcast(sendbuf, size, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* Print data that was received */
+    print_buffers(printbuf, sendbuf, size);
+
+    MPI_Finalize();
+    return 0;
+}
+
+
+void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize)
+{
+    int rank, i;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if(rank == 0)
+    {
+    for (i = 0; i < buffersize; i++) {
+        recvbuffer[i] = -1;
+        sendbuffer[i] = i;
+    }
+    }
+    else
+      {
+     for (i = 0; i < buffersize; i++) {
+        recvbuffer[i] = -1;
+        sendbuffer[i] = -1;
+    }
+    }
+}
+
+
+void print_buffers(int *printbuffer, int *sendbuffer, int buffersize)
+{
+    int i, j, rank, ntasks;
+
+    MPI_Gather(sendbuffer, buffersize, MPI_INT,
+               printbuffer, buffersize, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &ntasks);
+
+    if (rank == 0) {
+        for (j = 0; j < ntasks; j++) {
+            printf("Task %i:", j);
+            for (i = 0; i < buffersize; i++) {
+                printf(" %2i", printbuffer[i + buffersize * j]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+}
+
