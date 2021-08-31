d9a13affc6be85f77a8ca04593a4722efdd7278b
Cristian-Vasile Achim <66278390+csccva@users.noreply.github.com>
2021-08-31 16:19:19 +0300

    Update bcast.cpp

--- broadcast-scatter/cpp/solution/bcast.cpp
+++ broadcast-scatter/cpp/solution/bcast.cpp
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <mpi.h>
-
+#include <cstdio>
+#include <vector>
 
 void print_buffers(int *printbuffer, int *sendbuffer, int buffersize);
 void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize);
@@ -10,8 +11,10 @@ void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize);
 int main(int argc, char *argv[])
 {
     int ntasks, myid, color,size=12;
-    int sendbuf[ size], recvbuf[size];
-    int printbuf[size * size];
+    MPI_Status status;
+    std::vector<int> sendbuf(size);
+    std::vector<int> recvbuf(size);
+    std::vector<int> printbuf(size*size);
 
     MPI_Comm sub_comm;
 
@@ -20,30 +23,29 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
     /* Initialize message buffers */
-    init_buffers(sendbuf, recvbuf, size);
+    init_buffers(sendbuf.data(), recvbuf.data(), size);
 
     /* Print data that will be sent */
-    print_buffers(printbuf, sendbuf, size);
+    print_buffers(printbuf.data(), sendbuf.data(), size);
 
     
     /* Send  everywhere */
     if( myid ==0){
       for(int i=1; i<ntasks; i++){
-          MPI_Send(sendbuf, size, MPI_INT, i, i, MPI_COMM_WORLD);
+          MPI_Send(sendbuf.data(), size, MPI_INT, i, i, MPI_COMM_WORLD);
           }
      }
      else
      {
-       MPI_Recv(sendbuf, size, MPI_INT, 0, myid, MPI_COMM_WORLD, &status);
+       MPI_Recv(sendbuf.data(), size, MPI_INT, 0, myid, MPI_COMM_WORLD, &status);
      }
 
     /* Print data that was received */
-    print_buffers(printbuf, sendbuf, size);
+    print_buffers(printbuf.data(), sendbuf.data(), size);
 
     MPI_Finalize();
     return 0;
 }
-
 
 void init_buffers(int *sendbuffer, int *recvbuffer, int buffersize)
 {
@@ -87,4 +89,3 @@ void print_buffers(int *printbuffer, int *sendbuffer, int buffersize)
         printf("\n");
     }
 }
-
